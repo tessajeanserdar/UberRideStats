b10042bffdbb61872d02672fe399df8a9afbcd15
Tessa Serdar <tessajeanserdar@gmail.com>
2016-06-09 11:29:55 -0700

    updates callback url to /v1

--- server/lib.js
+++ server/lib.js
@@ -5,8 +5,8 @@ exports.UberInit = function(client_id,client_secret,server_token){
   client_id: client_id,
   client_secret: client_secret,
   server_token: server_token,
-  // redirect_uri: 'https://hidden-sea-86580.herokuapp.com/api/callback',
-  redirect_uri: 'http://localhost:3000/api/callback',
+  // redirect_uri: 'https://hidden-sea-86580.herokuapp.com/v1/callback',
+  redirect_uri: 'http://localhost:3000/v1/callback',
   name: 'RideStats.me',
   language: 'en_US'
 });
